a594f1e779fbaa0da6ca28ed2809bf9b87fc4dbb
jcardenas0917 <jcardenas@fixcityrepairs.com>
2022-04-24 20:11:58 -0400

    format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { CoreModule } from './../../projects/stepper-library/src/lib/core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './../../projects/stepper-library/src/lib/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StepperLibraryContainerComponent } from './components/stepper-library-container/stepper-library-container.component';
-import { DemoStepOneModule } from 'projects/stepper-library/src/lib/steps/demo-step-one/demo-step-one.module';
+
 
 @NgModule({
   declarations: [
